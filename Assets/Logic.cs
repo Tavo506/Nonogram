35e67df8040d2bef1c81776b67fced6bbc27473c
iFreuk <Freukgit.1>
2020-05-20 21:06:03 -0600

    Se agregaron los comentarios
    Se comentó

--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -3,10 +3,35 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Threading;
 using System.Collections.Generic;
-
+     /*
+        Gustavos Algorithm Picross Nonogram, blockMoving -     Last Update 05-20-2020  Completed on 40.4%
+        Authors:
+        Gustavo Alvarado Orozco
+        Gustavo Blanco Alfaro
+     
+        Curso:     Análisis de Algoritmos
+        Instituto: Tecnológico de Costa Rica
+        Educador:  Ywen Law
+     */
 public class Logic : MonoBehaviour
 {
-    int[,] matriz, copia;
+    /*
+        Acá se declaran las variables que usaremos dentro de nuestro proyecto
+        Matriz, corresponde a la matriz lógica de nuestro nonograma que almacena
+        vacios y llenos, a su vez gameObject Cubitos es su homólogo dentro de la parte gráfica.
+        
+        X y Y se usaran para definir el tamaño de la matriz, y sus limites.
+        UltimoAcomodo, será una variable global que estará guardadon la última posición donde se movió un bloque
+
+        Declaramos los estados que podemos tener en el nonograma, en este caso creamos un tercero que no estamos utilizando
+        en este algoritmo pero sí en otro.
+
+        casilla corresponde a la variable que va a estar contenida dentro de cubitos, es decir
+        la celda en sí
+
+        Filas y columnas, no son más que eso, las pistas por fila y columnas que hemos leído del txt
+    */
+    int[,] matriz;
     GameObject[,] cubitos;
     int x, y;
     int ultimoAcomodo;
@@ -20,9 +45,10 @@ public class Logic : MonoBehaviour
 
 
 
-    // Start is called before the first frame update
+    // Start funcion default de Unity
     void Start()
     {
+        //Acá sacamos todos los datos de txt y lo vamos leyendo / guardando
         string[] nonogram = System.IO.File.ReadAllLines(@"Assets\Nonogram.txt");
 
         x = int.Parse(nonogram[0].Split(',')[0]);
@@ -34,7 +60,7 @@ public class Logic : MonoBehaviour
          1 = pintado
          */
 
-
+        //Aca inicializamos la matriz con el tamaño leído
         matriz = new int[x, y];
 
         for (int n = 0; n < x; n++)
@@ -47,8 +73,10 @@ public class Logic : MonoBehaviour
 
         cubitos = new GameObject[x, y];
 
-        //Debug.Log("DIMENSIONES: " + x + ", " + y);
-
+        
+        
+        //Ésta función se encarga de leer los datos después de la palabra columnas y se encarga de separarlas
+        // y leerlas para posteriormente guardarlas donde corresponden
         int i = 2;
         while (nonogram[i] != "COLUMNAS")
         {
@@ -58,36 +86,19 @@ public class Logic : MonoBehaviour
         }
 
         i++;
-
+        //Esta parte del código es homologo al pequeño bloque anterior pero trabaja sobre Filas
         while (i < nonogram.Length)
         {
             string[] aux = nonogram[i].Replace(" ", "").Split(',');
             columnas.Add(aux);
             i++;
         }
-        /*
-        Debug.Log("FILAS");
-
-        foreach (string fila in filas)
-        {
-
-            Debug.Log("\t" + fila);
-        }
-
-        Debug.Log("COLUMNAS");
-
-        foreach (string columna in columnas)
-        {
-
-            Debug.Log("\t" + columna);
-        }
-        */
 
         StartCoroutine(NonogramStart());
 
     }
 
-
+    //Creamos la funcion que nos permitirá ir contando e ir paso a paso.
     IEnumerator NonogramStart()
     {
 
@@ -106,6 +117,7 @@ public class Logic : MonoBehaviour
         {
             for (int j = 0; j < x; j++)
             {
+                //Generamos la parte gráfica del nonograma
                 casilla = new GameObject("Casilla " + j + ", " + i);
                 casilla.transform.SetParent(this.transform);
                 casilla.AddComponent<SpriteRenderer>();
@@ -118,13 +130,13 @@ public class Logic : MonoBehaviour
     }
 
 
-
+    //Funcion que nos facilitará cambiar los sprites cuando queramos pasar
+    // de estado vacío a lleno
     void changeSprite(GameObject cuadro, Sprite estado)
     {
 
         cuadro.GetComponent<SpriteRenderer>().sprite = estado;
 
-        //cuadro.transform.name += " - " + estado.name;
     }
 
     /*
@@ -210,10 +222,12 @@ public class Logic : MonoBehaviour
      * Esto es solo para encontrar más rápido la función y no buscar tanto :v +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
      */
 
+    //Esta funcion primero comprueba que la columna cumpla las pistas y de no ser así
+    // jala bloques a para igualar la misma y en caso que después de jalar no haber podido igualar
+    // las pistas de las columnas comienza a llamar el backtrack
     void NonogramPuntoSolve(int columna, int fila)
     {
         if (!VerificaColumnas(columna)) {
-            copia = matriz;
             acomoda(columna, fila);
         }
         if (VerificaColumnas(columna)) 
@@ -222,8 +236,7 @@ public class Logic : MonoBehaviour
         }
         else
         {
-            //matriz = copia;
-            
+            //Aca va a empezar a backtrackear hasta que la columna esté bien acomodada
             int contador = 0;
             while (!VerificaColumnas(columna) && columna + 1 < y && fila + contador + 1 < x) { 
                 backway(columna + 1 ,fila+contador);
@@ -234,6 +247,7 @@ public class Logic : MonoBehaviour
         return;
     }
 
+    //Funcion que imprime la matriz, no es referenciada
     void imprimeMat() {
 
         for (int i = 0; i < x; i++) {
@@ -246,6 +260,10 @@ public class Logic : MonoBehaviour
     
     }
 
+    //Esta funcion se llama cuando no se puede igualar bloques con las pistas, recibe la columna anterior
+    //busca lo que decidí llamara cabezas de bloques, si encuentra una que se caracteriza por:
+    // tiene que ser un lleno y la celda a su derecha ser vacío.
+    // una vez encontrado lo vuelve a mover con moverBloque
     void backway(int col, int fila) 
     {
 
@@ -261,6 +279,12 @@ public class Logic : MonoBehaviour
         return;
     }
 
+    //Esta función debería volver a poner el bloque de caracteres lo más a la izquierda posible
+    // de y luego vuelve a llamar a la funcion NonogramPuntoSolve pero en este caso le dice que ignore la fila
+    // en la que se hizo el ultimo acomodo, es decir normalmente la más alta, va a ignorarla, para
+    // buscar algun bloque más inferior que igualmente cumpla con igualar la pista
+    // en caso de no encontrar cabezas de bloques o las que hay no se pueden mover, el programa vuelve a moverse
+    // de columna para repetir el proceso
     void moverBloque(int fila, int columna, int filaOrigen) 
     {
         int aux = buscarBloque(fila, columna);
@@ -274,7 +298,9 @@ public class Logic : MonoBehaviour
     }
 
 
-
+    //Esta funcion comienza a ser llamada desde la ultima columna, para ir cumpliendo las pistas de columnas
+    // intenta jalar el primer bloque a disposición para ir igualando a las pistas y encaso de igualar una
+    // da un salto de fila y comienza nuevamente con la siguiente pista.
     void acomoda(int columna, int inicio)
     {
         int cont = 0, indice = 0;
@@ -308,6 +334,10 @@ public class Logic : MonoBehaviour
         }
     }
 
+    // Esta funcion retrocede en caso de no tener donde poner las pistas que no las deje tiradas
+    // en caso de no poder poner un bloque que iguale la pista simplemente no pone la cadena
+    // de la columna por lo que no lo pone y NonogramPuntoSolve debería decir que no hay solucion con los bloques que tenemos
+    // y comenzar a llamar al backtraking
     int corrige(int fila, int columna)
     {
         while(matriz[fila, columna] == 1) {
@@ -323,6 +353,8 @@ public class Logic : MonoBehaviour
         return fila+1;
     }
 
+    //Esta funcion busca el primer bloque de llenos que se encuentre apartir de una columna y fila determinada
+    // los fija en vacío y cuenta de que tamaño es el bloque
     int buscarBloque(int fila, int columna)
     {
         int cont = 0;
@@ -347,6 +379,8 @@ public class Logic : MonoBehaviour
         return 0;
     }
 
+    //Usando la misma fila y columna que el anterior esta función recibe el tamaño del bloque y apartir de acá
+    // comienza a llenar las celdas del mismo tamaño que el bloque contado
     void ponerBloque(int fila, int columna, int bloque)
     {
         for(int j = bloque-1; j >= 0; j--)
@@ -357,6 +391,7 @@ public class Logic : MonoBehaviour
         }
     }
 
+    //Esta funcion es igual a la anterior pero coloca los bloques de izquiera a derecha
     void invPonerBloque(int fila, int columna, int bloque)
     {
         for (int j = columna; j < columna+bloque; j++)
@@ -367,6 +402,7 @@ public class Logic : MonoBehaviour
         }
     }
 
+    //esta funcion retorna la columna más a la izquierda posible donde se podría insertar un bloque de celdas llenas
     int dondePonerBack(int fila, int columna)
     {
         int aux = columna;
@@ -380,7 +416,8 @@ public class Logic : MonoBehaviour
         return aux+1;
     }
 
-
+    // Verifica que las pistas de las filas sean iguales a las pistas de las filas
+    //Se dejo de usar en este algoritmo porque siempre retornará true
     bool VerificaFilas(int i)
     {
         string recorredor = crearStringFila(i);
@@ -392,6 +429,7 @@ public class Logic : MonoBehaviour
         else return false;
     }
 
+    //Verifica que las coulmnas estén bien con respecto a sus pistas
     bool VerificaColumnas(int j)
     {
         string recorredor = crearStringColumna(j);
@@ -404,6 +442,7 @@ public class Logic : MonoBehaviour
             return false;
     }
 
+    //Funicon que cuenta las celdas llenas de una fila y las convierte en un string
     string crearStringFila(int i)
     {
         
@@ -426,6 +465,8 @@ public class Logic : MonoBehaviour
         return recorredor;
     }
 
+    //Funicon que cuenta las celdas llenas de una columna y las convierte en un string
+    // posterior mente se compara con el string que almacena las pistas
     string crearStringColumna(int j)
     {
         string recorredor = "";
@@ -464,6 +505,7 @@ public class Logic : MonoBehaviour
         }
     }
 
+    //Pregunta si una columna esta vacía
     bool columnaVacia(int j)
     {
         for(int i = 0; i < x; i++)
@@ -474,7 +516,7 @@ public class Logic : MonoBehaviour
         return true;
     }
 
-
+    //Nos imprime las pistas
     public void verPistas()
     {
         Debug.Log("FILAS");
@@ -492,6 +534,8 @@ public class Logic : MonoBehaviour
         }
     }
 
+    //Verfica con el algorimo de bloques que todas las verifica columnas sean correctos
+    // de ser así significa que pudimos solucionar el nonograma
     bool resuelto()
     {
         for(int i = 0; i < y; i++)
@@ -502,6 +546,8 @@ public class Logic : MonoBehaviour
         return true;
     }
 
+
+    //Funciones que nos permiten llamar de una u otra manera el programa.
     public void solveSinPausa()
     {
        resolverNonograma();
